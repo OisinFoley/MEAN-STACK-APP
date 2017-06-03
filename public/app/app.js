57ddba8bde67bb72fb2e082c7d9d99621adc0e9c
oisinfoley <oisinfoley@yahoo.co.uk>
2017-06-03 15:25:59 +0100

    new ngRoute branch, first commit

---
+++ public/app/app.js
@@ -0,0 +1,10 @@
+/* going to use this main config file to inject our other angular files into this main application */
+
+/* only injecting this one into our index (body ng-app etc..) as this module will feed/is linked to our other 
+angular modules ensure to add the routes.js as a dependency */
+angular.module('userApp',['appRoutes'])
+
+.config(function(){
+	console.log('testing main app config');	
+});
+
---
+++ public/app/routes.js
@@ -0,0 +1,23 @@
+// app.js(linked back to index itself) references this as a dependency.
+angular.module('appRoutes',['ngRoute'])
+
+.config(function($routeProvider, $locationProvider){
+//on index page, hrefs needs to be '#/abcd' because of Angular, normally just '/' will suffice
+	$routeProvider
+	.when('/', {
+		templateUrl: 'app/views/pages/home.html'
+	})
+	.when('/about', { //when copying&pasting, remember to actually add a directory!! Lost 40 minutes with this
+		templateUrl: 'app/views/pages/about.html'
+	})
+
+	.otherwise({ redirectTo: '/' }) //handles unrecognised values
+
+	//this, along with base href, will allow us to remove the '#' at the base url, currently needed because of Angular
+	$locationProvider.html5Mode({
+	  enabled: true,
+	  requireBase: false
+	});
+
+	//console.log('testing our routes file');
+});
